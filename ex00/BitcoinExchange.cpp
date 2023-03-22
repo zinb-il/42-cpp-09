60555721c66915c089a6fa3857cb5ac304c96af9
zinb <97052963+zinb-il@users.noreply.github.com>
2023-03-22 17:47:24 +0000

    cpp09

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: ziloughm <ziloughm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 16:52:28 by ziloughm          #+#    #+#             */
-/*   Updated: 2023/03/21 19:15:47 by ziloughm         ###   ########.fr       */
+/*   Updated: 2023/03/22 17:47:17 by ziloughm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,7 +68,7 @@ BitcoinExchange & BitcoinExchange::operator=(BitcoinExchange const & ob)
 /********************************************************************/
 
 
-std::map<std::string, int> BitcoinExchange::getBtc(void) const
+std::map<std::string, double> BitcoinExchange::getBtc(void) const
 {
     return (this->btc);
 }
@@ -100,19 +100,48 @@ const char * BitcoinExchange::ErrFile::what() const throw ()
 /*                          Public functions                        */
 /********************************************************************/
 
-bool BitcoinExchange::getValidDate(std::string s)
+// bool BitcoinExchange::getValidDate(std::string s)
+// {
+//     std::istringstream  date(s);
+//     struct              tm tm;
+//     int                 m,d,y;
+//     char                dash1, dash2;
+    
+//     if(date >> y >> dash1 >> m >> dash2 >> d && \
+//         dash1 == '-' && dash2 == '-' && date.eof())
+//         return true;
+//     std::cout << ERR1 << s << std::endl;
+//     return false;
+// }
+int * BitcoinExchange::getValidDate(std::string s)
 {
-    std::istringstream date(s);
-    int m,d,y;
-    char dash1, dash2;
-    if(date >> y >> dash1 >> m >> dash2 >> d && \
-        dash1 == '-' && dash2 == '-' && date.eof())
+    int                 *dt = new int [3];
+    std::smatch         match;
+    std::regex          re("^(\\d{4})-(\\d\\d)-(\\d\\d)$");
+    int                 daysInMonth[] = {31,28,31,30,31,30,31,31,30,31,30,31};
+    
+    if(std::regex_match(s, match, re))
     {
-        if (d <= 0 || d > 31 || m <= 0 && m > 12 || y < 0)
-            return false;
-        return true;
+        dt[2] = std::stoi(match[3]);
+        dt[1] = std::stoi(match[2]);
+        dt[0] = std::stoi(match[1]);
+        if (dt[1] == 2 && ((!(dt[0] % 4) && dt[0] % 100) || !(dt[0] %400)))
+            daysInMonth[1] = 29;
+        if (dt[0] > 0 && dt[1] > 0 && dt[1] <= 12 && dt[2] >= 1 && dt[2] <= daysInMonth[dt[1] - 1])
+            return dt;
     }
-    std::cout << ERR1 << date << std::endl;
+    std::cout << ERR1 << s << std::endl;
+    return 0;
+}
+
+bool BitcoinExchange::getValidNumb(long double n)
+{
+    if (n >= 0 && n <= 1000)
+        return true;
+    else if (n < 0)
+        std::cout << ERR3 << std::endl;
+    else
+        std::cout << ERR2 << std::endl;
     return false;
 }
 
@@ -124,24 +153,36 @@ void BitcoinExchange::getDataBase(void)
         throw (BitcoinExchange::ErrFile());
     std::getline(*db, str);
     while (std::getline(*db, str))
-        btc[str.substr(0, str.find(DEL1))] = std::stoi(str.substr(str.find(DEL1) + 1));
+        btc[str.substr(0, str.find(DEL1))] = std::stold(str.substr(str.find(DEL1) + 1));
+}
+
+void BitcoinExchange::getNextDate(int *dt, std::string date, long double num)
+{
+    std::str
+    std::string d = std::to_string(dt[0]) + "-" + std::to_string(dt[1]) + "-" + std::to_string(dt[3]);
+    while(!btc[date])
+    {
+        dt[3]--;
+        break;
+    }
+    std::cout << date << " => " << num << " = " << (num * btc[date]) << std::endl;
 }
 
 void BitcoinExchange::getCurrency(void)
 {
     std::string         str;
     std::string         date;
-    long int            num;
+    long double         num;
     
     std::getline(*inputfile, str);
     while (std::getline(*inputfile, str))
     {
         date = str.substr(0, str.find(DEL2));
-        num = std::stoi(str.substr(str.find(DEL2) + 3));
-        if (!getValidDate(date) || num < 1 || num > 10000)
-        {
-            std::cout << ERR1 << date << std::endl;
-        }
+        num = std::stold(str.substr(str.find(DEL2) + 3));
+        if (!getValidDate(date) || !getValidNumb(num))
+            continue;
+        else
+            getNextDate(getValidDate(date) ,date, num);
     }
 }
 
--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: ziloughm <ziloughm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 16:33:40 by ziloughm          #+#    #+#             */
-/*   Updated: 2023/03/21 19:08:20 by ziloughm         ###   ########.fr       */
+/*   Updated: 2023/03/22 17:43:57 by ziloughm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,20 @@
 # define BITCOINEXCHANGE_HPP
 
 # include <iostream>
+# include <iomanip> 
 # include <fstream>
 # include <sstream>
 # include <string>
 # include <map>
 # include <iterator>
 # include <algorithm> 
+# include <ctime>
+#include <regex>
 
 # define ERR "Error: could not open file."
 # define ERR1 "Error: bad input => "
 # define ERR2 "Error: too large a number."
+# define ERR3 "Error: not a positive number."
 # define DATA "data.csv"
 # define DEL1 ","
 # define DEL2 " | "
@@ -31,18 +35,20 @@
 class BitcoinExchange
 {
     private:
-        std::map<std::string, int> btc;
+        std::map<std::string, double> btc;
         std::string file_in;
         std::ifstream *inputfile;
         void getDataBase(void);
-        bool getValidDate(std::string s);
+        void getNextDate(int *dt, std::string date, long double num);
+        int  *getValidDate(std::string s);
+        bool getValidNumb(long double n);
         BitcoinExchange();
     public:
         BitcoinExchange(std::string file);
         BitcoinExchange(BitcoinExchange const & ob);
         ~BitcoinExchange();
         BitcoinExchange & operator=(BitcoinExchange const & ob);
-        std::map<std::string, int> getBtc(void) const;
+        std::map<std::string, double> getBtc(void) const;
         std::string getFile(void) const;
         std::ifstream * getInfile(void) const;
         void    getCurrency(void);
